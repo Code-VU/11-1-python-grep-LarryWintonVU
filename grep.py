8527b5cfc366b4089b3e7b8933da9b9c258ce3f3
Larry Winton <Larry.Winton@vu.com>
2022-04-12 10:53:30 -0500

    11.1 first try

--- grep.py
+++ grep.py
@@ -4,7 +4,16 @@ import sys
 
 
 def countpatterninfile():
+    fileName = 'mbox-long.txt'
     regular_expression = input("Enter a regular expression: ")
+    file_handle = open(fileName, mode='r', encoding='utf-8')
+    countOfMatchingLines = 0
+    for line in file_handle :
+        foundList = re.findall(regular_expression, line)
+        if (len(foundList) > 0) :
+            countOfMatchingLines = countOfMatchingLines + 1
+    print(fileName, 'had', countOfMatchingLines, 'lines that matched', regular_expression)
+
 
 if __name__ == '__main__':
     # this is called a main method
